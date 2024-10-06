5306225bc46476894b3bb54c245eddd07184f883
mariambuzaladze <mariambuzaladze10@example.come>
2024-10-06 21:19:58 +0400

    feat: add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import FormBuilder from "./pages/FormBuilder";
+import Result from "./pages/Result";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<FormBuilder />} />
+        <Route path="/" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
